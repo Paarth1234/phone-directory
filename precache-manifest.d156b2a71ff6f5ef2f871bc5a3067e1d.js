40f73923e056ad515f133453ddaee83ba9542160
Paarth <paarth171548.cse@chitkara.edu.in>
2019-12-09 18:24:43 +0530

    Updates

---
+++ precache-manifest.d156b2a71ff6f5ef2f871bc5a3067e1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8658515778c09bcebdbdced3c3a1fab",
+    "url": "/phone-directory/index.html"
+  },
+  {
+    "revision": "b0110049f8a519d4909b",
+    "url": "/phone-directory/static/css/main.ad7e3c29.chunk.css"
+  },
+  {
+    "revision": "ad88fc23cbbb8365a20c",
+    "url": "/phone-directory/static/js/2.0e4944c0.chunk.js"
+  },
+  {
+    "revision": "b0110049f8a519d4909b",
+    "url": "/phone-directory/static/js/main.6b24d715.chunk.js"
+  },
+  {
+    "revision": "123152631e28e7abc5f0",
+    "url": "/phone-directory/static/js/runtime-main.337be516.js"
+  }
+]);
