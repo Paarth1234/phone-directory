2ce6dc8f207d73429eab5cf872be858741f66c63
Paarth <paarth171548.cse@chitkara.edu.in>
2019-12-09 17:35:22 +0530

    Routing: Added routing for Add Subscriber page and Show Subscribers page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import AddSusbscriber from './AddSubscriber';
 import './common/common.css'
 import PhoneDirectory from './PhoneDirectory';
 
@@ -12,4 +11,4 @@ ReactDOM.render(<PhoneDirectory />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister(); 
